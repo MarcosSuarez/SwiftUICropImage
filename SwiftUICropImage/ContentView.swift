c1e0d3fbdfb4e24047b23b6401c690258c386c4e
Marcos Suárez <marcos.sua.aya@gmail.com>
2024-06-28 13:56:53 +0200

    Crop image

--- SwiftUICropImage/ContentView.swift
+++ SwiftUICropImage/ContentView.swift
@@ -7,18 +7,80 @@
 
 import SwiftUI
 
+struct ImageInfo: Identifiable {
+    let id: Int
+    let image: Image
+}
+
 struct ContentView: View {
+    
+    @State private var image: Image?
+    @State private var imagesCuts:[ImageInfo] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Cats!")
+                .font(.title)
+            
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            HStack(spacing: 8) {
+                ForEach(imagesCuts) { imageData in
+                    imageData.image
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            
+            Button {
+                imagesCuts.shuffle()
+            } label: {
+                Text("Shuffle")
+            }
+            
+            Spacer()
         }
         .padding()
+        .onAppear(perform: loadImage)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+private extension ContentView {
+    @MainActor
+    func loadImage() {
+        image = Image(.fourCats)
+        
+        guard let image,
+              let cgImg = ImageRenderer(content: image).cgImage
+        else { return }
+        
+        let numberOfImages: Int = 4
+        let sizeCGImage = cgImg.width/numberOfImages
+        
+        for index in 0..<numberOfImages {
+            let sizeCrop = CGSize(width: sizeCGImage, height: cgImg.height)
+            let cgRect = CGRect(origin: CGPoint(x: index*sizeCGImage, y: 0), size: sizeCrop)
+            
+            guard let cropImage =  cgImg.cropping(to: cgRect)
+            else { return }
+            
+            let newImage = convertToImage(cgImg: cropImage, width: CGFloat(cropImage.width), height: CGFloat(cropImage.height))
+            
+            imagesCuts.append(ImageInfo(id: index, image: newImage))
+        }
+    }
+    
+    func convertToImage(cgImg: CGImage, width: CGFloat, height: CGFloat) -> Image {
+#if (os(macOS))
+        Image(nsImage: NSImage(cgImage: cgImg, size: NSSize(width: width, height: height)))
+#else
+        Image(uiImage: UIImage(cgImage: cgImg))
+#endif
+    }
 }
