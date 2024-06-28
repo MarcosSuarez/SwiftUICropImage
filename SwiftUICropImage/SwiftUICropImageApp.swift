a4e7586e67fbbec50df9b68756091132de6be2f7
Marcos Suárez <marcos.sua.aya@gmail.com>
2024-06-28 13:54:38 +0200

    Initial Commit

---
+++ SwiftUICropImage/SwiftUICropImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICropImageApp.swift
+//  SwiftUICropImage
+//
+//  Created by Marcos Suarez Ayala on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICropImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
